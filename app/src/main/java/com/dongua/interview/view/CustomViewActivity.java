2b34fb85df6bcc459004672c00299c32a8fba821
Lewis <a549295838@163.com>
2019-01-18 14:54:30 +0800

    支持回弹处理的touchListener

--- app/src/main/java/com/dongua/interview/view/CustomViewActivity.java
+++ app/src/main/java/com/dongua/interview/view/CustomViewActivity.java
@@ -1,10 +1,15 @@
 package com.dongua.interview.view;
 
+import android.graphics.Rect;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
 
 import com.dongua.interview.R;
@@ -13,6 +18,8 @@ import com.example.common.BaseActivity;
 import java.util.ArrayList;
 
 import butterknife.BindView;
+
+import static android.view.View.OVER_SCROLL_NEVER;
 
 /**
  * @CreateDate: 2019/1/16 下午5:13
@@ -43,10 +50,112 @@ public class CustomViewActivity extends BaseActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         helper = new FlexibleAdapterHelper(recyclerView, adapter);
-        recyclerView.setAdapter(helper);
+        recyclerView.setAdapter(adapter);
+        //去掉滑动边界阴影
+        recyclerView.setOverScrollMode(OVER_SCROLL_NEVER);
+        recyclerView.setOnTouchListener(new UpDownScrollListener(true, true));
 
     }
 
+    private Rect topRect = new Rect();
+    // y方向上当前触摸点的前一次记录位置
+    private int previousY = 0;
+    // y方向上的触摸点的起始记录位置
+    private int startY = 0;
+    // y方向上的触摸点当前记录位置
+    private int currentY = 0;
+    // y方向上两次移动间移动的相对距离
+    private int deltaY = 0;
+
+    private float moveHeight;
+
+    private boolean handleTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startY = (int) event.getY();
+                previousY = startY;
+
+                topRect.set(recyclerView.getLeft(), recyclerView.getTop(),
+                        recyclerView.getRight(), recyclerView.getBottom());
+                Log.i("wxddd", "ACTION_DOWN: " + topRect.toShortString());
+                moveHeight = 0;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                currentY = (int) event.getY();
+                deltaY = currentY - previousY;
+                previousY = currentY;
+
+                //判定是否在顶部或者滑到了底部
+                if ((!recyclerView.canScrollVertically(-1) && (currentY - startY) > 0) || (!recyclerView.canScrollVertically(1) && (currentY - startY) < 0)) {
+                    //计算阻尼
+                    float distance = currentY - startY;
+                    if (distance < 0) {
+                        distance *= -1;
+                    }
+
+                    float damping = 0.5f;//阻尼值
+                    float height = recyclerView.getHeight();
+                    if (height != 0) {
+                        if (distance > height) {
+                            damping = 0;
+                        } else {
+                            damping = (height - distance) / height;
+                        }
+                    }
+                    if (currentY - startY < 0) {
+                        damping = 1 - damping;
+                    }
+
+                    //阻力值限制再0.3-0.5之间，平滑过度
+                    damping *= 0.25;
+                    damping += 0.25;
+
+                    moveHeight = moveHeight + (deltaY * damping);
+
+                    recyclerView.layout(topRect.left, (int) (topRect.top + moveHeight), topRect.right,
+                            (int) (topRect.bottom + moveHeight));
+
+
+
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (!topRect.isEmpty()) {
+                    //开始回移动画
+                    upDownMoveAnimation();
+                    // 子控件回到初始位置
+                    recyclerView.layout(topRect.left, topRect.top, topRect.right,
+                            topRect.bottom);
+                }
+                //重置一些参数
+                startY = 0;
+                currentY = 0;
+                topRect.setEmpty();
+                break;
+
+        }
+        return false;
+
+    }
+
+    private void upDownMoveAnimation() {
+        TranslateAnimation animation = new TranslateAnimation(0.0f, 0.0f,
+                recyclerView.getTop(), topRect.top);
+        animation.setDuration(600);
+        animation.setFillAfter(true);
+        //设置阻尼动画效果
+        animation.setInterpolator(new DampInterpolator());
+        recyclerView.setAnimation(animation);
+    }
+
+    public class DampInterpolator implements Interpolator {
+        @Override
+        public float getInterpolation(float input) {
+            //没看过源码，猜测是input是时间（0-1）,返回值应该是进度（0-1）
+            //先快后慢，为了更快更慢的效果，多乘了几次，现在这个效果比较满意
+            return 1 - (1 - input) * (1 - input) * (1 - input) * (1 - input) * (1 - input);
+        }
+    }
 
     class TestAdapter extends RecyclerView.Adapter {
 
---
+++ app/src/main/java/com/dongua/interview/view/UpDownScrollListener.java
@@ -0,0 +1,98 @@
+package com.dongua.interview.view;
+
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.TranslateAnimation;
+
+/**
+ * @CreateDate: 2019/1/18 上午10:51
+ * @Author: Lewis Weng
+ * @Description: 支持对控件添加回弹效果，对多指操作不做额外处理
+ * todo 支持左右滑动  更好的支持双指
+ */
+public class UpDownScrollListener implements View.OnTouchListener {
+
+    public static final float DEFAULT_DAMPING = 1F;
+
+    //是否打开上滑 下拉回弹效果
+    private boolean upElastic = false;
+    private boolean downElastic = false;
+    private Rect emptyRect;
+    private float startY;
+    private float endY;
+    private int deltaY;
+    private float damping = DEFAULT_DAMPING;//阻尼值
+    private boolean restore = false;
+
+    public UpDownScrollListener(boolean upElastic, boolean downElastic) {
+        this.upElastic = upElastic;
+        this.downElastic = downElastic;
+    }
+
+    public void setDamping(float damping) {
+        this.damping = damping;
+    }
+
+    //这里默认只对第一次点击的手指做处理，即getX(0),getY(0)
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (emptyRect == null) {
+                    //拿到view在父布局中的l t r b
+                    emptyRect = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+                }
+                startY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (!v.canScrollVertically(-1) && downElastic
+                        || !v.canScrollVertically(1) && upElastic) {
+
+                    endY = event.getY();
+                    //拖得越远 阻尼应该越大
+                    damping = calDamping(endY - startY);
+                    deltaY = (int) ((endY - startY) / damping + 0.5f);
+                    v.layout(emptyRect.left,
+                            emptyRect.top + deltaY,
+                            emptyRect.right,
+                            emptyRect.bottom + deltaY);
+                    restore = true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (restore) {
+                    restoreAnimation(v, emptyRect);
+                    v.layout(emptyRect.left,
+                            emptyRect.top,
+                            emptyRect.right,
+                            emptyRect.bottom);
+                    restore = false;
+                    damping = 2f;//恢复默认值
+                }
+                break;
+        }
+        return false;
+    }
+
+    //根据滑动距离计算阻尼值
+    //有必要改成可配置?
+    private float calDamping(float distance) {
+        if (distance < 200)
+            return DEFAULT_DAMPING;
+        return DEFAULT_DAMPING + (distance - 200) / 500;
+    }
+
+    private void restoreAnimation(View view, Rect rect) {
+        TranslateAnimation animation = new TranslateAnimation(
+                0.0f, 0.0f,
+                view.getTop(), rect.top);
+        animation.setDuration(500);
+        animation.setFillAfter(true);
+        //恢复的时候应该反过来，用减速插值
+        animation.setInterpolator(new DecelerateInterpolator());
+        view.setAnimation(animation);
+    }
+
+}
